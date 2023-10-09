ded6d040a683904a3ba182cc5ec1320dc1e71334
TiboMenardo <91217390+TiboMenardo@users.noreply.github.com>
2023-10-09 19:50:00 +0200

    Comment out code

--- test.py
+++ test.py
@@ -3,29 +3,29 @@ import random
 def say_hello_world():
     print("Hello, World!")
 
-def CheckHighestNumber(highest, lowest):
-    if (highest >= lowest):
-        return True
-    return False
+#def CheckHighestNumber(highest, lowest):
+#    if (highest >= lowest):
+#        return True
+#    return False
 
-def GuessRandomNumber(number, minimum, maximum):
-    if (CheckHighestNumber(maximum, minimum) is False):
-        minimum, maximum = maximum, minimum
-
-    guessed_number = random.randint(minimum, maximum)
+#def GuessRandomNumber(number, minimum, maximum):
+#    if (CheckHighestNumber(maximum, minimum) is False):
+#        minimum, maximum = maximum, minimum
+#
+#    guessed_number = random.randint(minimum, maximum)
+#    
+#    if guessed_number == number:
+#        return True
+#    else:
+#        return False
     
-    if guessed_number == number:
-        return True
-    else:
-        return False
-    
-def PrintCountToTen():
-    for i in range(11):
-        print(i)
+#def PrintCountToTen():
+#    for i in range(11):
+#        print(i)
 
 
 say_hello_world()
 
-print(GuessRandomNumber(2, 1, 5))
+#print(GuessRandomNumber(2, 1, 5))
 
-PrintCountToTen()
+#PrintCountToTen()
